2866d31a589fb1170e08723d17bacecf00cf6fc2
IbrohimjonJ <ibrohimjonjahongirov988@gmail.com>
2024-05-28 20:03:46 +0300

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer py-5 bg-white  footer-center text-base-content">
+      <aside>
+        <p>
+          Copyright © 2024  All rights reserved by <a href="https://www.khojiakbar.uz" >Khojiakbar.uz</a>
+        </p>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer;
